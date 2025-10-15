809ed096f11448b0f0ec1a527dd8a9b0f82b4ab8
Martin Njuguna <martin.njau@live.com>
2025-10-16 00:31:52 +0300

    Fix logging configuration to use multiple drivers for stack channel and improve connection string formatting

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
 
         'stack' => [
-            'driver' => 'stack',
+            'driver' => ['stack', 'daily'],
             'channels' => explode(',', (string) env('LOG_STACK', 'single')),
             'ignore_exceptions' => false,
         ],
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
